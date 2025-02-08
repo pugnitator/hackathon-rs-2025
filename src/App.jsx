10202fe30765ca05333c7c3b9763059f28c22680
pugnitator <pugnitator2000@gmail.com>
2025-02-08 14:51:53 +0300

    Add routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,15 @@
 import './App.css'
 import { MainPage } from './Pages/main-page/MainPage'
+import { Routes, Route } from 'react-router-dom'
 
 function App() {
   return (
     <>
-    <MainPage />
-    <div>наша приложенька, уиии</div>
+    <Routes>
+      <Route path='/' element={<MainPage />} />
+      <Route path='/teammate' element={<MainPage />} />  {/* подставить нужную страницу */}
+      <Route path='/favorites' element={<MainPage />} /> {/* подставить нужную страницу */}
+    </Routes>
     </>
   )
 }
